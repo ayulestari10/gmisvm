ced861a3226be7e33fdf9a0c8363d28965bcf8f0
ayulestari10 <lestariayu669@gmail.com>
2018-07-14 19:23:14 +0700

    peng peng

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 import zipfile
 from time import gmtime, strftime
-import os as az
+import os
 
 
 app = Flask(__name__)
@@ -13,6 +13,9 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
+	print(app.root_path)
+	print("ayu")
+
 	matrix = np.array([
 	    [1, 4, 3],
 	    [5, 2, 4],
@@ -39,18 +42,6 @@ def index():
 def hasil():
 	return render_template('index.html')
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if valid_login( request.form['username'],
-						request.form['password']):
-			return log_the_user_in(request.form['username']) 
-		else:
-			error = 'Invalid username or password'
-
-	return render_template('login.html', error = error)
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
 	if request.method == 'POST':
@@ -69,20 +60,31 @@ def home():
 # https://stackoverflow.com/questions/41965026/extracting-all-the-files-of-a-selected-extension-from-a-zipped-file
 @app.route('/pelatihan', methods=['GET', 'POST'])
 def pelatihan():
+	print(f"CWD: {os.getcwd()}")
+
 	if request.method == 'POST':
+		
 		f = request.files['zip_file']
 		filename = 'data/' + secure_filename(strftime("%Y-%m-%d-%H-%M-%S") + '_' + f.filename)
 		f.save(filename)
-		with open(filename, mode = 'r') as file:
-			zip_file = zipfile.ZipFile(filename)
-			files = [zip_file.extract(fl, 'data/training') for fl in zip_file.namelist()]
-			zip_file.close()
-		az.remove(filename)
 
-		# pelatihan disini
+		directory = strftime("%Y-%m-%d-%H-%M-%S")
 
-		flash('Data pelatihan berhasil dilatih')
-		return redirect(url_for('.pelatihan'))
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+			with open(filename, mode = 'r') as file:
+				zip_file = zipfile.ZipFile(filename)
+				files = [zip_file.extract(fl, 'data/training/' + directory) for fl in zip_file.namelist()]
+				zip_file.close()
+			os.remove(filename)
+
+			# pelatihan disini
+
+
+
+			flash('Data pelatihan berhasil dilatih')
+			return redirect(url_for('.pelatihan'))
 
 	return render_template('layout.html', data = { 'view' : 'pelatihan', 'title' : 'Pelatihan'})
 
