57975296f931c143fc7b01f8d3f88fba00adc13c
ayulestari10 <lestariayu669@gmail.com>
2018-09-15 15:15:25 +0700

    iniitt

--- libs/Deteksi_wajah.py
+++ libs/Deteksi_wajah.py
@@ -25,24 +25,148 @@ class Deteksi_wajah:
 			'marah': (255, 0, 0)
 		}
 
+	def get_sliding_window(self, matrix, x, y, size = 24):
+		return matrix[x:x+size, y:y+size]
+
+	def rectangle_a(self, sliding_window, x, y, size=(1, 2)):
+		A = sliding_window[x - 1, y - 1]
+		B = sliding_window[x - 1, (y + size[1]  - 1) // 2]
+		C = sliding_window[x + size[0] - 1, y - 1]
+		D = sliding_window[x + size[0] - 1, (y + size[1] - 1) // 2]
+
+		black = D + A - (B + C)
+
+		A = sliding_window[x - 1, ((y + size[1]) - 1) // 2]
+		B = sliding_window[x - 1, (y + size[1]) - 1]
+		C = sliding_window[x + size[0] - 1, ((y + size[1]) - 1) // 2]
+		D = sliding_window[x + size[0] - 1, (y + size[1]) - 1]
+
+		white = D + A - (B + C)
+
+		return abs(black - white)
+
+	def rectangle_b(self, sliding_window, x, y, size=(2, 1)):
+		A = sliding_window[x - 1, y - 1]
+		B = sliding_window[x - 1, y + size[1]  - 1]
+		C = sliding_window[(x + size[0] - 1) // 2, y - 1]
+		D = sliding_window[(x + size[0] - 1) // 2, y + size[1] - 1]
+
+		black = D + A - (B + C)
+
+		A = sliding_window[(x + size[0] - 1) // 2, y - 1]
+		B = sliding_window[(x + size[0] - 1) // 2, y + size[1] - 1]
+		C = sliding_window[x + size[0] - 1, y - 1]
+		D = sliding_window[x + size[0] - 1, (y + size[1]) - 1]
+
+		white = D + A - (B + C)
+
+		return abs(black - white)
+
+	def rectangle_c(self, sliding_window, x, y, size=(1, 3)):
+		chunk_size = size[1] // 3
+
+		ul_x = x - 1
+		ul_y = y - 1
+		ur_x = x - 1
+		ur_y = y + chunk_size - 1
+		bl_x = x + size[0] - 1
+		bl_y = y - 1
+		br_x = x + size[0] - 1
+		br_y = y + chunk_size - 1
+
+		A = sliding_window[ul_x, ul_y]
+		B = sliding_window[ur_x, ur_y]
+		C = sliding_window[bl_x, bl_y]
+		D = sliding_window[br_x, br_y]
+
+		black = D + A - (B + C)
+
+		A = sliding_window[ul_x, ul_y + chunk_size]
+		B = sliding_window[ur_x, ur_y + chunk_size]
+		C = sliding_window[bl_x, bl_y + chunk_size]
+		D = sliding_window[br_x, br_y + chunk_size]
+
+		white = D + A - (B + C)
+
+		A = sliding_window[ul_x, ul_y + (chunk_size * 2)]
+		B = sliding_window[ur_x, ur_y + (chunk_size * 2)]
+		C = sliding_window[bl_x, bl_y + (chunk_size * 2)]
+		D = sliding_window[br_x, br_y + (chunk_size * 2)]
+
+		black += D + A - (B + C)
+
+		return abs(black - white)
+
+	def rectangle_d(self, sliding_window, x, y, size=(2, 2)):
+		chunk_x = size[0] // 2
+		chunk_y = size[1] // 2
+
+		ul_x = x - 1
+		ul_y = y - 1
+		ur_x = x - 1
+		ur_y = y + chunk_y - 1
+		bl_x = x + chunk_x - 1
+		bl_y = y - 1
+		br_x = x + chunk_x - 1
+		br_y = y + chunk_y - 1
+
+		A = sliding_window[ul_x, ul_y]
+		B = sliding_window[ur_x, ur_y]
+		C = sliding_window[bl_x, bl_y]
+		D = sliding_window[br_x, br_y]
+
+		black = D + A - (B + C)
+
+		A = sliding_window[ul_x, ul_y + chunk_y]
+		B = sliding_window[ur_x, ur_y + chunk_y]
+		C = sliding_window[bl_x, bl_y + chunk_y]
+		D = sliding_window[br_x, br_y + chunk_y]
+
+		white = D + A - (B + C)
+
+		A = sliding_window[ul_x + chunk_x, ul_y]
+		B = sliding_window[ur_x + chunk_x, ur_y]
+		C = sliding_window[bl_x + chunk_x, bl_y]
+		D = sliding_window[br_x + chunk_x, br_y]
+
+		white += D + A - (B + C)
+
+		A = sliding_window[ul_x + chunk_x, ul_y + chunk_y]
+		B = sliding_window[ur_x + chunk_x, ur_y + chunk_y]
+		C = sliding_window[bl_x + chunk_x, bl_y + chunk_y]
+		D = sliding_window[br_x + chunk_x, br_y + chunk_y]
+
+		black += D + A - (B + C)
+		print(f"black: {D} + {A} - ({B} + {C})")
+
+		return abs(black - white)
+
 	def deteksi_wajah(self):
 		# Praproses Ke Gray Scale
 
 		image = "D:\\bahagia.png"
 
-		im = self.resize_image(image)
-
-		print(im)
+		# im = self.resize_image(image)
 
 		# im 	= Image.open(image)
 		# im	= im.convert('L') 
 		# im 	= np.array(im)
 
-		# im = np.array([
-		# 	[1, 2, 3],
-		# 	[2, 3, 4],
-		# 	[4, 5, 1]
-		# ])
+		# im = np.random.randint(255, size=(24, 24))
+		# print(im)
+		# sliding_window = self.get_sliding_window(im, 0, 0)
+		# integral_image = self.integral_image(sliding_window)
+
+		im = np.array([
+			[2, 6, 13, 18, 26],
+			[3, 12, 20, 32, 47],
+			[8, 23, 40, 57, 78],
+			[16, 40, 67, 90, 118],
+			[26, 62, 97, 123, 157]
+		])
+
+		print(self.rectangle_d(im, 1, 1, (2, 2)))
+
 
 		# im2 = np.array([
 		# 	[228,	10, 	10]
